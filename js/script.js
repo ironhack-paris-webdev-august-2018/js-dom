1648723daaa8436c7e8f0708be9f9845ddc993ba
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-08-10 14:47:28 +0200

    Activate delete buttons

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log("script.js CONNECTED!");
 var postCommentBtn = document.querySelector(".post-button");
 var commentList = document.querySelector("ol");
 var textBox = document.querySelector("input");
+var deleteButtons = document.querySelectorAll(".delete");
 
 // add a "click" event handler (when this event happens run this function)
 postCommentBtn.onclick = function () {
@@ -13,11 +14,33 @@ postCommentBtn.onclick = function () {
 
   // #2 set contents and styling of the tag
   // (use the input's value to get the user's text)
-  newLi.innerHTML = textBox.value + " <button>Delete</button>";
+  newLi.innerHTML = textBox.value + " <button class='delete'>Delete</button>";
 
   // #3 attach new tag to a parent that's in the document (puts it on the page)
   commentList.appendChild(newLi);
 
+  // activate the click functionality of the new delete button
+  var newButton = newLi.querySelector(".delete");
+  newButton.onclick = function () {
+    // get the parent of the <button> (the <li> tag)
+    var parentLi = newButton.parentNode;
+    console.log("delete coucou", parentLi);
+    // remove the parent from the DOM
+    parentLi.remove();
+  };
+
   // clear the form by setting input's value to empty
   textBox.value = "";
 };
+
+// loop over the list of delete buttons
+deleteButtons.forEach(function (oneBtn) {
+  // add a "click" event handler (when this event happens run this function)
+  oneBtn.onclick = function () {
+    // get the parent of the <button> (the <li> tag)
+    var parentLi = oneBtn.parentNode;
+    console.log("delete coucou", parentLi);
+    // remove the parent from the DOM
+    parentLi.remove();
+  };
+});
